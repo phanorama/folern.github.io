ff3af232189d9e6c7ea99d7fb0dfa1f3e881dbba
R1nNy44 <im.brindawan@gmail.com>
2021-12-04 00:07:39 +0800

    Typo & src=\"test.jpg\"

--- htmlpath/8. Images/asset/js/Descriptions.js
+++ htmlpath/8. Images/asset/js/Descriptions.js
@@ -2,11 +2,11 @@ let des = [
     {
     numb: 1,
     title: "Images ",
-    description: "Tag &lt;img&gt; digunakan untuk memasukkan gambar, <br> serta URL nya di definisikan dengan attribut src.Sebelum memasukkan gambar, kamu harus menaruh direktori foto tersebut, dengan cara<br><br> &lt; img src='direktori file' &gt;"
+    description: "Tag &lt;img&gt; digunakan untuk memasukkan gambar, <br> serta URL nya di definisikan dengan attribut src.Sebelum memasukkan gambar, kamu harus menaruh direktori foto tersebut, dengan cara<br><br> &lt; img src=\"direktori file\" &gt;"
     },
     {
     numb: 2,
     title: "Images Size ",
-    description: "Untuk mengubah ukuran gambar,  attribut pakai height dan width,dengan memakai satuan px (pixel) maupun %.<br> contoh penggunaannya seperti,<br><br> &lt;img src='test.jpg' height='100px' width='100px' &gt;. <br><br>Kamu juga bisa menambah border di gambar yang dimasukkan, <br> dengan meletakkan attribut border diikuti ukurannya dengan px."
+    description: "Untuk mengubah ukuran gambar,  attribut pakai height dan width,dengan memakai satuan px (pixel) maupun %.<br> contoh penggunaannya seperti,<br><br> &lt;img src=\"test.jpg\" height='100px' width='100px' &gt;. <br><br>Kamu juga bisa menambah border di gambar yang dimasukkan, <br> dengan meletakkan attribut border diikuti ukurannya dengan px."
     }
     ]
